0c018d4f6c01fae0a3a6e81b2a68d9559b0cbe40
Rafael Velho Garcia <rafaelvelhogarcia13@gmail.com>
2023-04-18 11:29:41 -0300

    ex016.py
    Exercício Python 16: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.

---
+++ ex016.py
@@ -0,0 +1,13 @@
+#Exercício Python 16: Crie um programa que leia um número Real qualquer pelo teclado
+#e mostre na tela a sua porção Inteira.
+
+'''from math import trunc
+v = float(input('Digite um valor'))
+
+print('O valor digitado foi {} e sua porçao inteira é {}'.format(v,(trunc(v))))'''
+
+v = float(input('Digite um número'))
+print('O valor digitado foi {} e sua porçao inteira é {}'.format(v,(int(v))))
+
+
+
