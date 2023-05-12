f27f6bb08be168cf7d0fcbfa946e6fc976e81bcc
Rafael Velho Garcia <rafaelvelhogarcia13@gmail.com>
2023-05-12 07:58:57 -0300

    ex033
    Programa que ordena uma lista de números

---
+++ ex033.py
@@ -0,0 +1,10 @@
+'''a = float(input('Digite o primeiro valor:'))
+b = float(input('Digite o segundo valor'))
+c = float(input('Digite o terceiro valor'))
+d = float(input('Digite o quarto numero:'))
+lista = [a,b,c,d]
+lista.sort()
+print('O menor valor digitado foi {}'.format(lista[0]))
+print('O maior valor digitado foi {}'.format(lista[-1]))'''
+
+
